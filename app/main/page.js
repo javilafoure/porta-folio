a8f3e7bfd54ce50b5ed1fefbf9deaaf3c1b6927b
JcReyes29 <j.creyesp29@gmail.com>
2024-10-14 12:37:53 -0500

    page-main

---
+++ app/main/page.js
@@ -0,0 +1,15 @@
+export default function Main() {
+    return (
+        <div className="bg-gradient-to-r from-slate-800 to-yellow-500  w-full h-screen mx-auto grid justify-center items-center">
+            <div className="text-4xl gap-10 grid w-96">
+                <h2>Hi, I am</h2>
+                <h1 className="flex justify-center font-bold font-sans text-5xl">DEVELOPER</h1>
+                <div className="text-lg flex gap-5">
+                    <h2>WEB DEVELOPER</h2>
+                    <span> UI & UX DESIGNER</span>
+                </div>
+            </div>
+
+        </div>
+    )
+}
